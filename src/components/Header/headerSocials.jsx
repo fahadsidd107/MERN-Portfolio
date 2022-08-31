32d1f0662c42e34a4773144011525ea8457f64a9
fahadsidd107 <fsiddiqui107@gmail.com>
2022-08-31 06:02:03 +0500

    work

---
+++ src/components/Header/headerSocials.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BsLinkedin, BsGithub, BsTwitter } from 'react-icons/bs'
+
+function headerSocials() {
+  return (
+    <div className='header__socials'>
+      <a href='https://www.linkedin.com/in/fahad-siddiqui-595484176/' target='_blank' className=''><BsLinkedin /></a>
+      <a href='https://github.com/fahadsidd107' target='_blank' className=''><BsGithub /></a>
+      <a href='https://twitter.com/Fad_Flex' target='_blank' className=''><BsTwitter /></a>
+    </div>
+  )
+}
+
+export default headerSocials
