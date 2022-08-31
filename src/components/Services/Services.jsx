61797e5d17eb1e57bd22b8f1f0fb8f7ab9b32fef
fahadsidd107 <fsiddiqui107@gmail.com>
2022-08-31 06:00:59 +0500

    work

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Services.css'
+
+const Services = () => {
+  return (
+    
+    <section id='services'>
+      <h5>What  I offer</h5>
+      <h2>Services</h2>
+      <div className='container services__container'>
+
+      </div>
+
+    </section>
+  )
+}
+
+export default Services
