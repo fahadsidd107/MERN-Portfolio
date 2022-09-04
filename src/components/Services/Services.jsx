50d64b25ccd4271745383b2fee5f23e8becc98dc
fahadsidd107 <fsiddiqui107@gmail.com>
2022-09-04 21:25:20 +0500

    work

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -10,7 +10,7 @@ const Services = (para) => {
       <div className='container services__container'>
 
 
-        <article className='services'>
+        <article className='service'>
           <div className='service__head'>
             <h3>UI/UX Design</h3>
           </div>
@@ -25,7 +25,7 @@ const Services = (para) => {
         {/* End of UI/UX */}
 
 
-        <article className='services'>
+        <article className='service'>
           <div className='service__head'>
             <h3>Web Development</h3>
           </div>
@@ -40,7 +40,7 @@ const Services = (para) => {
         {/* End of Web Development */}
 
 
-        <article className='services'>
+        <article className='service'>
           <div className='service__head'>
             <h3>Content Creation</h3>
           </div>
