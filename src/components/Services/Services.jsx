41269ea215ebd20a4b4c43d07f07cfada487c706
fahadsidd107 <fsiddiqui107@gmail.com>
2022-09-17 16:12:42 +0500

    work

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -30,7 +30,7 @@ const Services = (para) => {
           <ListItem para="lorem ipsum qsw wfh yhrtew rgrrewr yt rthtr " />
           <ListItem para="lorem ipsum qsw wfh yhrtew rgrrewr yt rthtr " />
           <ListItem para="lorem ipsum qsw wfh yhrtew rgrrewr yt rthtr " />
- 
+
         </article>
         {/* End of Web Development */}
 
