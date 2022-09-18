bf5bc8361d43cc0e8cbe92f80b80ec19432f13db
fahadsidd107 <fsiddiqui107@gmail.com>
2022-09-18 13:36:19 +0500

    work

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,13 +4,13 @@ import './Portfolio.css'
 const Portfolio = () => {
   return (
     <section id='portfolio'>
-   <h5>My Recent Work</h5>
-   <h2>Porfolio</h2>
-   <div className='container portfolio_container'>
-    <article className='portfolio_item'>
+      <h5>My Recent Work</h5>
+      <h2>Porfolio</h2>
+      <div className='container portfolio_container'>
+        <article className='portfolio_item'>
 
-    </article>
-   </div>
+        </article>
+      </div>
     </section>
   )
 }
