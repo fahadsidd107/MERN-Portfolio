642377c97a9f2d413e9ba072f9c25f706a854845
fahadsidd107 <fsiddiqui107@gmail.com>
2022-09-18 13:36:08 +0500

    work

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -7,7 +7,9 @@ const Portfolio = () => {
    <h5>My Recent Work</h5>
    <h2>Porfolio</h2>
    <div className='container portfolio_container'>
-    
+    <article className='portfolio_item'>
+
+    </article>
    </div>
     </section>
   )
