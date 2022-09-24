a5cebb7570d1cd6a5857f9280596c933cfb4aab2
fahadsidd107 <fsiddiqui107@gmail.com>
2022-09-24 16:14:39 +0500

    work

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,7 +4,7 @@ import IMG1 from '../../assets/portfolio1.jpg'
 import IMG2 from '../../assets/portfolio2.jpg'
 import IMG3 from '../../assets/portfolio3.jpg'
 import IMG4 from '../../assets/portfolio4.jpg'
-import IMG1 from '../../assets/portfolio1.jpg'
+import IMG5 from '../../assets/portfolio5.jpg'
 import IMG1 from '../../assets/portfolio1.jpg'
 
 const Portfolio = () => {
