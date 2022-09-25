8d4fb6ac5719242d5b5cd2669ac9f5aba9779ef8
fahadsidd107 <fsiddiqui107@gmail.com>
2022-09-25 23:07:13 +0500

    work

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,7 +4,7 @@ import IMG1 from '../../assets/portfolio1.jpg'
 import IMG2 from '../../assets/portfolio2.jpg'
 import IMG3 from '../../assets/portfolio3.jpg'
 import IMG4 from '../../assets/portfolio4.jpg'
-import IMG5 from '../../assets/portfolio5.jpg'
+import IMG5 from '../../assets/portfolio5.png'
 import IMG6 from '../../assets/portfolio6.jpg'
 
 const Portfolio = () => {
@@ -21,6 +21,14 @@ const Portfolio = () => {
           <a href='https://github.com/fahadsidd107' className='btn' target='_blank'>Github</a>
           <a href='https://github.com/fahadsidd107' className='btn btn-primary' target='_blank' >Live Demo</a>
 
+        </article>
+        <article className='portfolio_item'>
+          <div className='portfolio_item_image'>
+            <img src={IMG1} alt="IMG1" />
+          </div>
+          <h3>This is postfolio item title</h3>
+          <a href='https://github.com/fahadsidd107' className='btn' target='_blank'>Github</a>
+          <a href='https://github.com/fahadsidd107' className='btn btn-primary' target='_blank' >Live Demo</a>
         </article>
       </div>
     </section>
