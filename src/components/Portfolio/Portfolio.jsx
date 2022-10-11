9ffbc18dde96e6207863dd937032b5c8d3892016
fahadsidd107 <fsiddiqui107@gmail.com>
2022-10-11 21:16:24 +0500

    work

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -22,15 +22,6 @@ const Portfolio = () => {
         <Article image={IMG5} poster='Suiiii' github='https://github.com/fahadsidd107' demo='https://github.com/fahadsidd107' />
         <article className='portfolio_item'>
           <div className='portfolio_item_image'>
-            <img src={IMG4} alt="IMG4" />
-          </div>
-          <h3>This is postfolio item title</h3>
-          <a href='https://github.com/fahadsidd107' className='btn' target='_blank'>Github</a>
-          <a href='https://github.com/fahadsidd107' className='btn btn-primary' target='_blank' >Live Demo</a>
-
-        </article>
-        <article className='portfolio_item'>
-          <div className='portfolio_item_image'>
             <img src={IMG5} alt="IMG5" />
           </div>
           <h3>This is postfolio item title</h3>
