825b2eef5e9893f51ef696d12996576c3e5ee833
fahadsidd107 <fsiddiqui107@gmail.com>
2022-10-15 16:44:33 +0500

    work

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -8,7 +8,7 @@ import IMG5 from '../../assets/portfolio5.png'
 import IMG6 from '../../assets/portfolio6.jpg'
 
 
-const Portfolio = ({poster, title, github, demo }) => {
+const Portfolio = () => {
   return (
     <section id='portfolio'>
       <h5>My Recent Work</h5>
